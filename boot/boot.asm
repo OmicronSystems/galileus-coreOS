a17dc5ead5af648811d80d7c3ac0494d39ff32fa
Francesco Grecucci <grec.francesco@gmail.com>
2018-10-04 17:03:59 +0200

    Actually working but no Kernel boot.

--- boot/boot.asm
+++ boot/boot.asm
@@ -10,21 +10,21 @@
 ; | |__| | |  | |_| || |____| | \ \| |__| | |\  |  ____) |  | |  ____) |  | |  | |____| |  | |____) |
 ;  \____/|_|  |_|_____\_____|_|  \_\\____/|_| \_| |_____/   |_| |_____/   |_|  |______|_|  |_|_____/ 
 
-[org 0x7c00]  
+[org 0x7c00]
 
 KERNEL_OFFSET equ 0x1000		; This is the memory offset to which we will load our kernel
 
-		
+
 	mov [BOOT_DRIVE], dl		; BIOS stores our boot drive in DL.
-	
+
 	mov bp, 0x9000				; Set-up the stack
 	mov sp, bp
-	
+
 	call load_kernel
 
 	; thanks to https://stackoverflow.com/questions/32701854/boot-loader-doesnt-jump-to-kernel-code/32705076#32705076
 	; for solving the triple fault error when jumping into protected mode
-	
+
 	xor ax,ax      ; We want a segment of 0 for DS for this question
 	mov ds,ax      ; Set AX to appropriate segment value for your situation
 	mov es,ax      ; In this case we'll default to ES=DS
@@ -36,17 +36,16 @@ KERNEL_OFFSET equ 0x1000		; This is the memory offset to which we will load our 
 	sti            ; Re-enable interrupts
 
 	cld            ; Set the direction flag to be positive direction
-	
+
 	; end of the solution
 
 	call clrscr
 	call svga
 
 	call switch_to_pm	; Switch to 32 bit protected mode, we'll never come back here.
-	
+
 	jmp $
-	
-    	
+
 	; Load Kernel Function
 	load_kernel:
 		mov bx, KERNEL_OFFSET
@@ -64,20 +63,18 @@ KERNEL_OFFSET equ 0x1000		; This is the memory offset to which we will load our 
 [bits 32]
 
 	BEGIN_PM:
-	
+
 	mov ebx, MSG_LOADING
 	call print_string_pm
 	jmp $
-	
 
 	mov ebx, MSG_COPYRIGHT
 	call print_string_pm
 	jmp $
-	
+
 	mov ebx, MSG_LICENSE
 	call print_string_pm
 	jmp $
-		
 
 	call KERNEL_OFFSET	                ; Now jump to the address of our loaded
 						; kernel code, assume the brace position, 
@@ -87,6 +84,6 @@ KERNEL_OFFSET equ 0x1000		; This is the memory offset to which we will load our 
 	MSG_COPYRIGHT db 	'Copyright (c) 2018, Francesco Grecucci and Omicron Systems                      '
 	MSG_LICENSE db   	'NO WARRANTY. Licensed under the GNU v2 License', 0x00
 	BOOT_DRIVE db 0
-		
+
 	times 510-($-$$) db 0
 	dw 0xAA55
--- boot/disk_load.asm
+++ boot/disk_load.asm
@@ -13,28 +13,22 @@
 ; load DH sectors in ES:BX from drive DL
 
 disk_load:
-	push dx				; Store DX on stack so later we can recall
+	push dx					; Store DX on stack so later we can recall
 						; how many sectors were request to be read,
 						; even if it is altered in the meantime	
-	mov ah, 0x02		; BIOS read sector function
-	mov al, dh			; Read DH sectors
-	mov ch, 0x00		; Select cylinder 0
-	mov dh, 0x00		; Select head 0
-	mov cl, 0x02		; Start reading from second sector ( i.e.
+	mov ah, 0x02				; BIOS read sector function
+	mov al, dh				; Read DH sectors
+	mov ch, 0x00				; Select cylinder 0
+	mov dh, 0x00				; Select head 0
+	mov cl, 0x02				; Start reading from second sector ( i.e.
 						; after the boot sector )
-	int 0x13			; BIOS interrupt
-	
-	jc disk_error		; Jump if error ( i.e. carry flag set )
-	
-	pop dx				; Restore DX from the stack
-	cmp dh, al			; if AL ( sectors read ) != DH ( sectors expected )	
-	jne disk_error		;    display error message
-	ret
-	
-disk_error :
-	int 10h
-	mov ah, 09h
-	mov al, 'D'
-	mov bl, 0x74
-	hlt
+	int 0x13				; BIOS interrupt
+	jc disk_error				; Jump if error ( i.e. carry flag set )
 
+	pop dx					; Restore DX from the stack
+	cmp dh, al				; if AL ( sectors read ) != DH ( sectors expected )	
+	jne disk_error				; error procedure
+	ret
+
+disk_error :
+	hlt
